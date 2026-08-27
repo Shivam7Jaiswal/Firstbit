45d37bec29c9626ca29a0022f168d9d86f5a038f
Shivam Jaiswal <jaiswalss2002@gmail.com>
2026-08-27 22:47:48 +0530

    Add files via upload

---
+++ Assignment 1/Q5.c
@@ -0,0 +1,14 @@
+// write a program  to check whether the person is eligible to vote age =<18
+
+void main()
+{
+	int age=10;
+	if (age>=18)
+	{
+	printf("the person is eligible to vote");
+    }
+     else
+     {
+     	printf("the person is not eligible to vote");
+	 }
+}
---
+++ Assignment 1/Q6.c
@@ -0,0 +1,15 @@
+// write a program to check whether a given character is uppercase or lowercase
+
+void main()
+{
+	char CH='b';
+	
+	if (CH >='a' )
+	{
+	printf("lower case");
+    }
+	else 
+	{
+		printf("upper case");
+	 } 
+}
